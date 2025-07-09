4c0dc567994506fed353088170ae761eb869ec5b
venkatesh221024 <venkatesh221024@gmail.com>
2025-07-09 06:33:48 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,21 +1,51 @@
 variable "filename" {
-    default = "file2.txt"
-   type = string 
+  default = "pets.txt"
+  type    = string
 }
 variable "content" {
-default = "Hi chanti this is variable task44"
-type = string
+  default = "This is pets information with ariables"
+  type    = string
 }
 variable "prefix" {
-    default = "mr"
-    type = string
+  default = "mrs"
+  type    = string
 }
-variable "separator" {
-    default = "."
-    type = string
-}
-
 variable "length" {
   default = "12"
-  type = number
+  type    = number
+}
+variable "pet_count" {
+  type    = number
+  default = "5"
+
+}
+variable "has_pets" {
+  type    = bool
+  default = true
+}
+variable "list" {
+
+  type    = list(string)
+  default = ["cat,dog"]
+}
+variable "pet_map" {
+  type = map(string)
+  default = {
+    "pet1" = "cat"
+    "pet2" = "dog"
+  }
+}
+variable "pet_info" {
+
+  type = object({
+    name  = string
+    age   = number
+    breed = string
+  })
+
+  default = {
+    name  = "fin"
+    age   = 2
+    breed = "labrador"
+  }
 }

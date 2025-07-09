bf8953b21fef1638db7ff6cc7b4a7eed84b180f8
venkatesh221024 <venkatesh221024@gmail.com>
2025-07-09 21:59:48 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -14,38 +14,3 @@ variable "length" {
   default = "12"
   type    = number
 }
-variable "pet_count" {
-  type    = number
-  default = "5"
-
-}
-variable "has_pets" {
-  type    = bool
-  default = true
-}
-variable "list" {
-
-  type    = list(string)
-  default = ["cat,dog"]
-}
-variable "pet_map" {
-  type = map(string)
-  default = {
-    "pet1" = "cat"
-    "pet2" = "dog"
-  }
-}
-variable "pet_info" {
-
-  type = object({
-    name  = string
-    age   = number
-    breed = string
-  })
-
-  default = {
-    name  = "fin"
-    age   = 2
-    breed = "labrador"
-  }
-}
